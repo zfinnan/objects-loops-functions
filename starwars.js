26741f2207d67ceb95cc6f08c74533480232b1cf
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-10-21 15:35:59 -0400

    [js] Created an object

---
+++ starwars.js
@@ -0,0 +1,9 @@
+const starwars = {
+    image: 'The Mandalorian',
+    length: '1:00',
+    title: "The Mandalorian",
+    views: '1M',
+    uploaded: 'One Day Ago'
+}
+
+console.log(starwars.title);
